5e9313f4da74a831d24c48838ca8e14b3322ae26
Anna Fedyna <annafedyna03@gmail.com>
2024-02-08 17:26:11 +0000

    Active entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
+def convert_md_to_html(filename):
+    content = util.get_entry(filename)
+    if not content:
+        return None
+    else:
+        markdowner = markdown2.Markdown()
+        return markdowner.convert(content)
 
 def index(request):
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    if convert_md_to_html(title) == None:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+            })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            'title': title,
+            'content' : convert_md_to_html(title)
+        })
